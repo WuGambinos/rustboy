683f8c0eb7e26072484d5270058d67da9e2f6f63
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2022-02-07 16:59:26 -0500

    First Commit

---
+++ src/chip.rs
@@ -0,0 +1,85 @@
+struct Flags {
+    zero_flag: u8,
+    sub_flag: u8,
+    half_carry_flag: u8,
+    carry_flag: u8,
+}
+
+impl Flags {
+    fn new() -> Self {
+        Flags {
+            zero_flag: 0,
+            sub_flag: 0,
+            half_carry_flag: 0,
+            carry_flag: 0,
+        }
+    }
+}
+
+pub struct GB {
+
+    memory: [u8; 65536],
+
+    ///Accumulator
+    a: u8,
+
+    ///Flags
+    f: Flags,
+
+    ///B register
+    b: u8,
+
+    ///C register
+    c: u8,
+
+    ///D register
+    d: u8,
+
+    ///E register
+    e: u8,
+
+    ///H register
+    h: u8,
+
+    ///L register
+    l: u8,
+
+    ///Stack pointer
+    sp: u8,
+
+    ///Program counter
+    pc: u8,
+}
+
+impl GB {
+    pub fn new() -> Self {
+        GB {
+            memory: [0; 65536],
+            a: 0,
+            f: Flags::new(),
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            h: 0,
+            l: 0,
+            sp: 0,
+            pc: 0,
+        }
+
+    }
+
+    fn emulate_cycle(&self) {
+
+    }
+
+    fn load_program(&mut self, rom: &[u8]) {
+
+    }
+
+    fn load_boot(&mut self, rom: &[u8]) {
+
+    }
+
+
+}
