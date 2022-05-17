e8ca7461ad9feef82591140bf4c4b0b9423790af
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2022-05-17 06:46:51 -0400

    Making progress with debugging

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ pub use mmu::*;
 
 use std::fs;
 use std::path::Path;
+use crate::cpu::instructions::{cp_r_r, inc_16bit, ld_8bit, ld_a_from_io, or_r_r};
 
 #[macro_use]
 extern crate text_io;
 
 fn main() {
-    let test_rom = "roms/cpu_instrs/individual/01-special.gb";
+    let test_rom = "roms/cpu_instrs/individual/09-op_rr.gb";
     let file_name = "roms/tetris.gb";
 
     //Path to rom
@@ -34,111 +35,106 @@ fn main() {
     let mut counter = 0;
 
 
-    loop {
-        cpu.emulate_cycle(&mut mmu);
-
-        if mmu.read_mem(0xFF02) == 0x81 {
-            let c: char = mmu.read_mem(0xFF01) as char;
-            print!("{}", c);
-
-            mmu.write_mem(0xff02, 0x0);
-        }
-
-    }
-
-    //Emulate a cpu cycle
-    //loop {
-    //cpu.emulate_cycle(&mut mmu);
 
 
-    /*if cpu.pc == 0xC7B8 {
-            println!();
-            cpu.print_status(&mmu);
-            println!("Counter: {}", counter);
-        }*/
+  /*for i in 0..9 {
 
-    /*for _ in 0..10 {
-        while cpu.pc != 0xC7B8 {
+
+        while  cpu.pc != 0xC05A {
             cpu.emulate_cycle(&mut mmu);
             counter += 1;
-        };
+        }
 
-        cpu.print_status(&mmu);
-        println!();
+      cpu.print_state(&mmu);
+      println!("COUNTER: {}", counter);
+      println!("{}-ith Iteration", i+1);
+      println!();
 
-        if mmu.read_mem(0xFF02) == 0x81 {
+      cpu.emulate_cycle(&mut mmu);
+
+        /*if mmu.read_mem(0xFF02) == 0x81 {
             let c: char = mmu.read_mem(0xFF01) as char;
             print!("{}", c);
-            if c == 'F' {
-                break;
-            }
-
             mmu.write_mem(0xff02, 0x0);
-        }
-
-        cpu.emulate_cycle(&mut mmu);
-    }
-
-    cpu.print_status(&mmu);
-
-    for i in 0..2 {
-        while cpu.pc != 0xC0AA {
-            cpu.emulate_cycle(&mut mmu);
-        }
-
-        println!();
-        cpu.print_status(&mut mmu);
-
-        if i != 1 {
-            cpu.emulate_cycle(&mut mmu);
-        }
+        }*/
 
     }
-    /*for i in 0..1100 {
+
+    cpu.print_state(&mmu);
+    println!("COUNTER: {}", counter);
+    println!();
+
+    let mut n = 31;
+    for _ in 0..n {
         cpu.emulate_cycle(&mut mmu);
         println!();
-        cpu.print_status(&mmu);
+        cpu.print_state(&mmu);
     }*/
 
-    println!();
-cpu.print_status(&mmu);
+
+    //n = 250;
+
+    /*for  _ in 0..(n*18) {
+        cpu.emulate_cycle(&mut mmu);
+        println!();
+        cpu.print_state(&mmu);
+    }*/
+
+    /*for _ in 0..30 {
+        while cpu.pc != 0xC00C {
+            cpu.emulate_cycle(&mut mmu);
+        }
+
+        //println!();
+        //cpu.print_state(&mmu);
+
+        cpu.emulate_cycle(&mut mmu);
+    }*/
+
+    /*for _ in 0..17 {
+        while cpu.pc != 0xC01C {
+            cpu.emulate_cycle(&mut mmu);
+        }
+        println!();
+        cpu.print_state(&mmu);
+
+        cpu.emulate_cycle(&mut mmu);
+
+    }*/
+
+    let mut cond = true;
+    while cond {
+
+        while cpu.pc != 0xC2EA {
+            cpu.emulate_cycle(&mut mmu);
+            cond = false;
+        }
+    }
+
+    cpu.print_state(&mmu);
+    mmu.write_mem(0xDFFD, 0xD0);
+
 
     loop {
         cpu.emulate_cycle(&mut mmu);
         println!();
-        cpu.print_status(&mmu);
-    }*/
+        cpu.print_state(&mmu);
+    }
 
-    //}
 
-        /*for _ in 0..16 {
-        while cpu.pc != 0x20D {
+    /*for _ in 0..30{
+        while cpu.pc != 0xC00C {
             cpu.emulate_cycle(&mut mmu);
         }
 
-        //cpu.print_status(&mmu);
+        println!();
+        cpu.print_state(&mmu);
+
         cpu.emulate_cycle(&mut mmu);
-        //println!();
-    }
-    cpu.emulate_cycle(&mut mmu);*/
 
-
-        //8814
-        /*let mut count = 0;
-
-    let end = 180630;
-    let old_end = 364250;
-
-    for _ in 0..61 {
-        cpu.emulate_cycle(&mut mmu);
-        /*cpu.print_status(&mmu);
-        println!();*/
-        if mmu.read_mem(0xFF02) == 0x81 {
-            let c: char = mmu.read_mem(0xFF01) as char;
-            print!("{}", c);
-            mmu.write_mem(0xff02, 0x0);
-        }
     }*/
+
+
 
 }
 
@@ -146,3 +142,6 @@ fn read_file(path: &Path) -> Result<Vec<u8>, std::io::Error> {
     //Reads file contents into vector
     fs::read(path)
 }
+
+
+
