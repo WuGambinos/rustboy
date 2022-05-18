8a435e4d434534dd5de71a3d0d2a7414dadb1b9c
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2022-05-18 01:19:02 -0400

    Added list to see which blaargs tests i have passed and failed

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use crate::cpu::instructions::{cp_r_r, inc_16bit, ld_8bit, ld_a_from_io, or_r_r}
 extern crate text_io;
 
 fn main() {
-    let test_rom = "roms/cpu_instrs/individual/09-op_rr.gb";
+    let test_rom = "roms/cpu_instrs/individual/06-ld r,r.gb";
     let file_name = "roms/tetris.gb";
 
     //Path to rom
@@ -36,20 +36,12 @@ fn main() {
 
 
    loop {
-       if cpu.opcode == 0x10 {
-           cpu.print_state(&mut mmu);
-           break;
-       }
        cpu.emulate_cycle(&mut mmu);
-       //println!();
-
-
-
-       /*if mmu.read_mem(0xFF02) == 0x81 {
+       if mmu.read_mem(0xFF02) == 0x81 {
             let c: char = mmu.read_mem(0xFF01) as char;
             print!("{}", c);
             mmu.write_mem(0xff02, 0x0);
-        }*/
+        }
    }
 }
 
