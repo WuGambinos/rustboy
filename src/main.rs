e33feac28e53f0d90385ed70dcdc5b7374133cb0
WuGambinos <lajuan.station@gmail.com>
2022-05-20 19:24:16 -0400

    Fixed rst function and now rst instruction works properly, only failing opcode in test 07-jr,jp,call,ret,rst is 0xD9 which involves interrupts so we'll have to come back to that

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::path::Path;
 extern crate text_io;
 
 fn main() {
-    let test_rom = "roms/cpu_instrs/individual/03-op sp,hl.gb";
+    let test_rom = "roms/cpu_instrs/individual/07-jr,jp,call,ret,rst.gb";
     let file_name = "roms/tetris.gb";
 
     //Path to rom
