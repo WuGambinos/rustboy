09c632675e33ca54de5b833f0aa86adf3f5b7e99
WuGambinos <lajuan.station@gmail.com>
2022-05-24 04:55:50 -0400

    Trying to find a smooth way to integrate timer

--- src/cpu/timer.rs
+++ src/cpu/timer.rs
@@ -1,5 +1,6 @@
 use crate::Mmu;
 
+
 pub struct Timer {
     /// Divider Register
     div: u16,
@@ -13,7 +14,7 @@ pub struct Timer {
 
 
 impl Timer {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             div: 0xAC00,
             tima: 0,
@@ -22,13 +23,47 @@ impl Timer {
         }
     }
 
-    fn timer_tick(&self) {
+    fn timer_tick(&mut self) {
+
+        let prev_div:  u16 = self.div;
+
+        self.div += 1;
+
+        let mut timer_update: bool = false;
+
+        match self.tac & 0b11 {
+            0b00 => timer_update = ((prev_div & (1 << 9)) == 1) && ((!(self.div & (1 << 9))) == 1),
+
+            0b01 => timer_update = ((prev_div & (1 << 3)) == 1) && ((!(self.div & (1 << 3))) == 1),
+
+            0b10 => timer_update = ((prev_div & (1 << 5)) == 1) && ((!(self.div & (1 << 5))) == 1),
+
+            0b11 => timer_update = ((prev_div & (1 << 7)) == 1) && ((!(self.div & (1 << 7))) == 1) ,
+
+            _ => (),
+        }
+
+        let cond: u8 = self.tac & (1 << 2);
+
+        if timer_update && cond == 1 {
+            self.tima += 1;
+
+
+            //If Overflow request interrupt
+            if self.tima == 0xFF {
+                self.tima = self.tima
+
+                //Request Timer interrupt
+
+
+            }
+        }
 
 
     }
 
     /// Read u8 valeu from Timer/Divider register at addr
-    fn timer_read(&self, mmu: &Mmu) -> u8 {
+    fn timer_read(&self, mmu: &Mmu, addr: u16) -> u8 {
         match addr {
             0xFF04 => return ((self.div as u16) >> 8) as u8,
 
@@ -38,7 +73,7 @@ impl Timer {
 
             0xFF07 => return self.tac,
 
-            _ => -1,
+            _ => 123,
         }
 
     }
@@ -59,7 +94,7 @@ impl Timer {
             //TAC
             0xFF07 => self.tac = value,
 
-            _ => "Not  a Timer/Divider Regsiter"
+            _ => ()
         }
 
     }
