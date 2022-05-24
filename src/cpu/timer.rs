eafe8a4b8b53353041a5ddd6f163e5f67e3a4d8a
WuGambinos <lajuan.station@gmail.com>
2022-05-24 04:35:52 -0400

    Rewriting my original timer idea

--- src/cpu/timer.rs
+++ src/cpu/timer.rs
@@ -1,42 +1,68 @@
-
-
-pub struct Clock {
-    main: u8,
-    sub: u8,
-    div: u8,
-}
-
-pub struct Reg {
-    /// DIV (Divider Register)
-    div: u8,
-    /// TIMA (Timer Counter)
-    tima: u8,
-    /// TMA (Timer Modulo)
-    tma: u8,
-    /// TAC (Timer Control)
-    tac: u8,
-}
+use crate::Mmu;
 
 pub struct Timer {
-    clock: Clock,
-    reg: Reg,
+    /// Divider Register
+    div: u16,
+    /// Timer Counter(R/W)
+    tima: u8,
+    /// Timer Modulo (R/W)
+    tma: u8,
+    ///Timer Control (R/W)
+    tac: u8,
 }
 
 
 impl Timer {
-    fn new(&self) -> Self {
+    fn new() -> Self {
         Self {
-            clock: Clock { main: 0, sub: 0, div: 0 },
-            reg: Reg { div: 0, tima: 0, tma: 0, tac: 0 },
+            div: 0xAC00,
+            tima: 0,
+            tma: 0,
+            tac: 0,
         }
     }
 
-    fn inc(&self)  {
-        //Increment by  the last opcode's time
+    fn timer_tick(&self) {
 
-        //No opcodes take longer then 4 M-times
-        //So we only need to check for overflow once
+
+    }
+
+    /// Read u8 valeu from Timer/Divider register at addr
+    fn timer_read(&self, mmu: &Mmu) -> u8 {
+        match addr {
+            0xFF04 => return ((self.div as u16) >> 8) as u8,
+
+            0xFF05 => return self.tima,
+
+            0xFF06 => return self.tma,
+
+            0xFF07 => return self.tac,
+
+            _ => -1,
+        }
+
+    }
+
+    /// Write u8 value to Timer/Divider register at addr
+    fn timer_write(&mut self, mmu: &Mmu, addr: u16, value: u8){
+        match addr{
+
+            //DIV
+            0xFF04 => self.div = 0,
+
+            //TIMA
+            0xFF05 => self.tima = value,
+
+            //TMA
+            0xFF06 => self.tma = value,
+
+            //TAC
+            0xFF07 => self.tac = value,
+
+            _ => "Not  a Timer/Divider Regsiter"
+        }
 
     }
 
 }
+
