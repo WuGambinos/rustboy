347b7efcfa22e44bc795ca1d00101492956fb29d
WuGambinos <lajuan.station@gmail.com>
2022-05-28 09:34:06 +0000

    Made game boy struct that contains mmu, gpu, and timer. So i can connect them. Also moved execute_instruction to impl Gameboy

--- src/main.rs
+++ src/main.rs
@@ -24,69 +24,30 @@ fn main() {
     //Contents of rom
     let rom: Vec<u8> = read_file(&rom_path).unwrap();
 
-    //Cpu
-    let mut cpu = Cpu::new();
+    let mut game_boy: GameBoy = GameBoy::new();
 
-    //Timer
-    let mut timer = Timer::new();
-
-    //Mapped Memory Unit
-    let mut mmu: Mmu = Mmu::new();
-
-    let mut game_boy: GameBoy = GameBoy::new(&mut cpu, &mut mmu, &mut timer);
+    game_boy.cpu.pc = 0x100;
 
     //Read Rom into memory
-    mmu.read_rom(&rom);
+    game_boy.mmu.read_rom(&rom);
 
-    cpu.pc = 0x100;
-
-    let mut counter = 0;
-
-    let mut cond = true;
-
-    /* while cond {
-        cpu.execute_instruction(&mut mmu);
-        if cpu.pc == 0x0050 {
-            cond = false;
-        }
-    }*/
-
-    //cpu.print_state(&mmu);
-
-    /* let mut counter = 0;
-
-    for _ in 0..189691 {
-        cpu.execute_instruction(&mut mmu);
-    }
-    mmu.write_mem(0xDFFB, 0xC0);
-    mmu.write_mem(0xDFFC, 0xC2);
-    cpu.print_state(&mmu);
-
-    println!();
-    cpu.execute_instruction(&mut mmu);
-
-    println!();
-    cpu.print_state(&mmu);
-
-    for _ in 0..13 {
-        cpu.execute_instruction(&mut mmu);
-        println!();
-        cpu.print_state(&mmu);
-    }*/
+    game_boy.cpu.pc = 0x100;
 
     loop {
-        cpu.execute_instruction(&mut mmu);
-        let cycles_passed = (cpu.timer.internal_ticks - cpu.last_cycle) * 4;
-        timer.do_cycle(cycles_passed);
+        game_boy.execute_instruction();
+        let cycles_passed = (game_boy.cpu.timer.internal_ticks - game_boy.cpu.last_cycle) * 4;
+        game_boy.timer.do_cycle(cycles_passed);
 
-        if mmu.read_mem(0xFF02) == 0x81 {
-            let c: char = mmu.read_mem(0xFF01) as char;
+        /*if game_boy.mmu.read_mem(0xFF02) == 0x81 {
+            let c: char = game_boy.mmu.read_mem(0xFF01) as char;
             print!("{}", c);
-            mmu.write_mem(0xff02, 0x0);
-        }
+            game_boy.mmu.write_mem(0xff02, 0x0);
+        }*/
 
-        /*("OPCODE: {:#X} CYCLE PASSED: {}", cpu.opcode, cycles_passed);
-        println!();*/
+        //("OPCODE: {:#X} CYCLE PASSED: {}", cpu.opcode, cycles_passed);
+        game_boy.cpu.print_state(&game_boy.mmu);
+        println!();
+        println!();
     }
 }
 
