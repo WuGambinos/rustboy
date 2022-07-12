c02a71adadf6437d1d4678dffabaa3f7899496f1
WuGambinos <lajuan.station@gmail.com>
2022-07-11 22:55:08 -0400

    Setting up ppu, thinking about pausing and adding more documentation

--- src/ppu.rs
+++ src/ppu.rs
@@ -1,14 +1,28 @@
-pub struct PPU {
-    vram: [u8; 0x2000]
+pub struct OamEntry {
+
+    ///Sprite's vertical position on the screen + 16
+    y: u8,
+
+    ///Sprite's horizontal position on the screen + 8
+    x: u8,
+
+    //
+    tile: u8,
 }
 
 
+impl OamEntry {
+    pub fn new() {}
+}
+
+pub struct PPU {
+    vram: [u8; 0x2000],
+}
+
 impl PPU {
-    pub fn new()  {
-
+    pub fn new() -> Self {
+        Self { vram: [0; 0x2000] }
     }
 
-    pub fn tick() {
-
-    }
+    pub fn tick() {}
 }
