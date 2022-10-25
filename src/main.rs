c5ccb35a57334021b4a058ce6e8ce4ab5ccaf7f4
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2022-10-25 19:26:11 -0400

    Removed unused code

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,6 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use std::time::Duration;
 
-//use raylib::prelude::*;
-
 fn main() {
     const SCREEN_WIDTH: u32 = 1024;
     const SCREEN_HEIGHT: u32 = 768;
@@ -81,25 +79,6 @@ fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     loop {
-        // GAME LOOP GOES HERE
-        /*if game_boy.cpu.pc > 0x100 {
-            'running: loop {
-                canvas.clear();
-                for event in event_pump.poll_iter() {
-                    match event {
-                        Event::Quit { .. }
-                        | Event::KeyDown {
-                            keycode: Some(Keycode::Escape),
-                            ..
-                        } => break 'running,
-                        _ => {}
-                    }
-                }
-                debug_window(&mut canvas, &game_boy.interconnect);
-
-                canvas.present();
-            }
-        }*/
         if !game_boy.cpu.halted {
             game_boy.cpu.execute_instruction(&mut game_boy.interconnect);
             if game_boy.interconnect.read_mem(0xFF02) == 0x81 {
@@ -145,7 +124,6 @@ fn read_file(path: &Path) -> Result<Vec<u8>, std::io::Error> {
 }
 
 static SCALE: i32 = 4;
-//static TILE_COLORS: [Color; 4] = [Color::WHITE, Color::GRAY, Color::DARKGRAY, Color::BLACK];
 static TILE_COLORS: [Color; 4] = [
     Color::RGB(255, 255, 255),
     Color::RGB(169, 169, 169),
@@ -256,100 +234,3 @@ fn display_tile(
         }
     }
 }
-/*fn debug_window(canvas: &mut RaylibDrawHandle, interconnect: &Interconnect) {
-    let mut x_draw = 0;
-    let mut y_draw = 0;
-    let mut tile_num = 0;
-
-    let _w: u32 = 16 * 8 * 4;
-    let _h: u32 = 32 * 8 * 4;
-
-    canvas.clear_background(Color::WHITE);
-    let addr: u16 = 0x8000;
-
-    for y in 0..24 {
-        for x in 0..16 {
-            display_tile(
-                canvas,
-                interconnect,
-                addr,
-                tile_num,
-                (x_draw + (x * SCALE)) as u32,
-                (y_draw + (y * SCALE)) as u32,
-            );
-            x_draw += 8 * SCALE;
-            tile_num += 1;
-        }
-        y_draw += 8 * SCALE;
-        x_draw = 0;
-    }
-}
-
-fn display_tile(
-    canvas: &mut RaylibDrawHandle,
-    interconnect: &Interconnect,
-    start_loc: u16,
-    tile_num: u16,
-    x: u32,
-    y: u32,
-) {
-    for tile_y in (0..16).step_by(2) {
-        let addr: u16 = start_loc + (tile_num * 16) + tile_y;
-
-        /*let b1: u8 = interconnect.read_mem(addr);
-        let b2: u8 = interconnect.read_mem(addr + 1);
-
-        for bit in (0..7).rev() {
-            let mut hi: u8 = b1 & (1 << bit) << 1;
-            let mut lo: u8 = b2 & (1 << bit);
-
-            hi = (hi == 0) as u8;
-            hi = (hi == 0) as u8;
-            hi = hi << 1;
-
-            lo = (lo == 0) as u8;
-            lo = (lo == 0) as u8;
-
-            let color: u8 = hi | lo;
-
-            let new_x = (x as i32) + ((7 - bit) * SCALE);
-            let new_y = (y as i32) + ((tile_y as i32) / 2 * SCALE);
-
-            let w = SCALE as i32;
-            let h = SCALE as i32;
-            //println!("ADDR_VALUE: {:#X} ADDR1_VALUE {:#X} ADDR: {:#X} ADDR+1: {:#X} HI: {:#X} LO: {:#X} COLOR: {}",b1, b2, addr, addr +1, hi, lo, color);
-            canvas.draw_rectangle(new_x, new_y, w, h, TILE_COLORS[color as usize]);
-        }*/
-        // Get First BYTE
-        let second_byte: u8 = interconnect.read_mem(addr);
-
-        // Get Second BYTE
-        let first_byte: u8 = interconnect.read_mem(addr + 1);
-
-        // Index for tile color
-        let mut color: u8 = 0;
-
-        // Iterate over bits of first and second byte
-        for bit in (0..7).rev() {
-            let first_bit = (first_byte >> bit) & 1;
-            let second_bit = (second_byte >> bit) & 1;
-            if first_bit == 0 && second_bit == 0 {
-                color = 0
-            } else if first_bit == 0 && second_bit == 1 {
-                color = 1;
-            } else if first_bit == 1 && second_bit == 0 {
-                color = 2
-            } else {
-                color = 3;
-            }
-
-            let new_x = (x as i32) + ((7 - bit) * SCALE);
-            let new_y = (y as i32) + ((tile_y as i32) / 2 * SCALE);
-
-            let w = SCALE as i32;
-            let h = SCALE as i32;
-            canvas.draw_rectangle(new_x, new_y, w, h, TILE_COLORS[color as usize]);
-        }
-    }
-}
-*/
