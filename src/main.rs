5a76594868fcc87328734554de033ff4be5c4c0c
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2022-11-09 17:38:14 -0500

    Moved sdl window functions to separate file. Also still trying to make drawing more efficient

--- src/main.rs
+++ src/main.rs
@@ -21,22 +21,19 @@ use std::path::Path;
 extern crate text_io;
 
 extern crate sdl2;
+use sdl2::*;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use std::time::Duration;
 
+const SCALE: i32 = 4;
+const SCREEN_WIDTH: u32 = 16 * 8 * (SCALE as u32); 
+const SCREEN_HEIGHT: u32 = 32 * 8 * (SCALE as u32);
 fn main() {
-    const SCREEN_WIDTH: u32 = 1024;
-    const SCREEN_HEIGHT: u32 = 768;
-    const SCALE: i32 = 4;
-
-    //const DEBUG_WIDTH: i32 = 16 * 8 * SCALE;
-    //const DEBUG_HEIGHT: i32 = 32 * 8 * SCALE;
-
     // Command Line Arguments
     let args: Vec<String> = env::args().collect();
-    let test_rom = args[1].as_str();
+    //let test_rom = args[1].as_str();
+    let test_rom = "roms/blaargs/cpu_instrs/individual/01-special.gb";
     let boot_rom = "roms/blaargs/boot-rom.gb";
 
     // Path to rom
@@ -63,23 +60,14 @@ fn main() {
     // Put PC at beginning of ROM
     game_boy.cpu.pc = 0x100;
 
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem
-        .window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
+    let sdl_context = sdl2::init().expect("Failed to start SDL");
+    let mut canvas = window::init_window(&sdl_context);
+    let mut event_pump = sdl_context.event_pump().expect("Failed to get event pump");
 
-    let mut canvas: WindowCanvas = window.into_canvas().build().unwrap();
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut j = 0;
+
     'running: loop {
-        canvas.clear();
         if !game_boy.cpu.halted {
             game_boy.cpu.execute_instruction(&mut game_boy.interconnect);
             if game_boy.interconnect.read_mem(0xFF02) == 0x81 {
@@ -96,7 +84,6 @@ fn main() {
                 game_boy.cpu.halted = false;
             }
         }
-        println!("J: {}", j);
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -107,53 +94,7 @@ fn main() {
                 _ => {}
             }
         }
-        debug_window(&mut canvas, &game_boy.interconnect);
-
-        canvas.present();
-        j += 1;
-    }
-
-    let mut i = 0;
-
-    loop {
-        /*'running: loop {
-            canvas.clear();
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit { .. }
-                    | Event::KeyDown {
-                        keycode: Some(Keycode::Escape),
-                        ..
-                    } => break 'running,
-                    _ => {}
-                }
-            }
-            debug_window(&mut canvas, &game_boy.interconnect);
-
-            canvas.present();
-        }*/
-
-        if !game_boy.cpu.halted {
-            game_boy.cpu.execute_instruction(&mut game_boy.interconnect);
-            if game_boy.interconnect.read_mem(0xFF02) == 0x81 {
-                let c: char = game_boy.interconnect.read_mem(0xFF01) as char;
-                print!("{}", c);
-                if c == 'd' {
-                    println!("I: {}", i);
-                    break;
-                }
-                game_boy.interconnect.write_mem(0xff02, 0x0);
-            }
-        } else {
-            game_boy.interconnect.emu_cycles(1);
-
-            let IF = game_boy.interconnect.read_mem(0xFF0F);
-
-            if IF != 0 {
-                game_boy.cpu.halted = false;
-            }
-        }
-        i += 1; 
+        window::debug_window(&mut canvas, &game_boy.interconnect);
     }
 }
 
@@ -164,115 +105,3 @@ fn read_file(path: &Path) -> Result<Vec<u8>, std::io::Error> {
     fs::read(path)
 }
 
-static SCALE: i32 = 4;
-static TILE_COLORS: [Color; 4] = [
-    Color::RGB(255, 255, 255),
-    Color::RGB(169, 169, 169),
-    Color::RGB(84, 84, 84),
-    Color::RGB(0, 0, 0),
-];
-
-fn debug_window(canvas: &mut WindowCanvas, interconnect: &Interconnect) {
-    let mut x_draw = 0;
-    let mut y_draw = 0;
-    let mut tile_num = 0;
-
-    let _w: u32 = 16 * 8 * 4;
-    let _h: u32 = 32 * 8 * 4;
-
-    canvas.set_draw_color(Color::RGB(17, 17, 17));
-    canvas.clear();
-    canvas.fill_rect(sdl2::rect::Rect::new(0, 0, 800, 600));
-    let addr: u16 = 0x8000;
-
-    for y in 0..24 {
-        for x in 0..16 {
-            display_tile(
-                canvas,
-                interconnect,
-                addr,
-                tile_num,
-                (x_draw + (x * SCALE)) as u32,
-                (y_draw + (y * SCALE)) as u32,
-            );
-            x_draw += 8 * SCALE;
-            tile_num += 1;
-        }
-        y_draw += 8 * SCALE;
-        x_draw = 0;
-    }
-}
-
-fn display_tile(
-    canvas: &mut WindowCanvas,
-    interconnect: &Interconnect,
-    start_loc: u16,
-    tile_num: u16,
-    x: u32,
-    y: u32,
-) {
-    for tile_y in (0..16).step_by(2) {
-        let addr: u16 = start_loc + (tile_num * 16) + tile_y;
-
-        /*let b1: u8 = interconnect.read_mem(addr);
-            let b2: u8 = interconnect.read_mem(addr + 1);
-
-            for bit in (0..7).rev() {
-                let mut hi: u8 = b1 & (1 << bit) << 1;
-                let mut lo: u8 = b2 & (1 << bit);
-
-                hi = (hi == 0) as u8;
-                hi = (hi == 0) as u8;
-                hi = hi << 1;
-
-                lo = (lo == 0) as u8;
-                lo = (lo == 0) as u8;
-
-                let color: u8 = hi | lo;
-
-                let new_x = (x as i32) + ((7 - bit) * SCALE);
-                let new_y = (y as i32) + ((tile_y as i32) / 2 * SCALE);
-
-                let w = SCALE as i32;
-                let h = SCALE as i32;
-                //println!("ADDR_VALUE: {:#X} ADDR1_VALUE {:#X} ADDR: {:#X} ADDR+1: {:#X} HI: {:#X} LO: {:#X} COLOR: {}",b1, b2, addr, addr +1, hi, lo, color);
-            canvas.draw_rectangle(new_x, new_y, w, h, TILE_COLORS[color as usize]);
-            p
-        }*/
-        // Get First BYTE
-        let second_byte: u8 = interconnect.read_mem(addr);
-
-        // Get Second BYTE
-        let first_byte: u8 = interconnect.read_mem(addr + 1);
-
-        // Index for tile color
-        let mut color: u8 = 0;
-
-        // Iterate over bits of first and second byte
-        for bit in (0..8).rev() {
-            let first_bit = (first_byte >> bit) & 1;
-            let second_bit = (second_byte >> bit) & 1;
-            if first_bit == 0 && second_bit == 0 {
-                color = 0
-            } else if first_bit == 0 && second_bit == 1 {
-                color = 1;
-            } else if first_bit == 1 && second_bit == 0 {
-                color = 2
-            } else {
-                color = 3;
-            }
-
-            let new_x = (x as i32) + ((7 - bit) * SCALE);
-            let new_y = (y as i32) + ((tile_y as i32) / 2 * SCALE);
-
-            let w = SCALE as u32;
-            let h = SCALE as u32;
-            //canvas.draw_rectangle(new_x, new_y, w, h, TILE_COLORS[color as usize]);
-            //canvas.set_draw_color(TILE_COLORS[color as usize]);
-            //canvas.fill_rect(sdl2::rect::Rect::new(w, h, new_x, new_y));
-            canvas.set_draw_color(TILE_COLORS[color as usize]);
-            //canvas.fill_rect(sdl2::rect::Rect::new(10, 10, 400, 400));
-            canvas.fill_rect(sdl2::rect::Rect::new(new_x, new_y, w, h));
-        }
-    }
-}
--- src/window.rs
+++ src/window.rs
@@ -1,23 +1,50 @@
 use crate::interconnect::Interconnect;
-use raylib::prelude::*;
 
-static tile_colors: [Color; 4] = [Color::WHITE, Color::GRAY, Color::DARKGRAY, Color::BLACK];
+use sdl2::*;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::render::WindowCanvas;
 
-static SCALE: i32 = 4;
+const SCALE: i32 = 4;
+const SCREEN_WIDTH: u32 = 16 * 8 * (SCALE as u32); 
+const SCREEN_HEIGHT: u32 = 32 * 8 * (SCALE as u32);
 
-pub fn update_debug_window(canvas: &mut RaylibDrawHandle, interconnect: &Interconnect) {
+static TILE_COLORS: [Color; 4] = [
+    Color::RGB(255, 255, 255),
+    Color::RGB(169, 169, 169),
+    Color::RGB(84, 84, 84),
+    Color::RGB(0, 0, 0),
+];
+
+pub fn init_window(sdl_context: &Sdl) -> WindowCanvas {
+    let video_subsystem = sdl_context.video().expect("failed to access subsystem");
+    let window = video_subsystem
+        .window("rust-sdl2 demo", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .position_centered()
+        .build()
+        .expect("failed to create window");
+
+    let canvas: WindowCanvas = window
+        .into_canvas()
+        .build()
+        .expect("failed to get sdl canvas");
+    
+    canvas
+}
+
+pub fn debug_window(canvas: &mut WindowCanvas, interconnect: &Interconnect) {
     let mut x_draw = 0;
     let mut y_draw = 0;
     let mut tile_num = 0;
 
-    let w: u32 = 16 * 8 * 4;
-    let h: u32 = 32 * 8 * 4;
+    let _w: u32 = 16 * 8 * 4;
+    let _h: u32 = 32 * 8 * 4;
 
-    //canvas.clear_background(Color::GRAY);
-    canvas.clear_background(Color::WHITE);
+    canvas.set_draw_color(Color::RGB(17, 17, 17));
+    //canvas.clear();
+    canvas.fill_rect(sdl2::rect::Rect::new(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
     let addr: u16 = 0x8000;
-
-    // 384 tiles 24x16
 
     for y in 0..24 {
         for x in 0..16 {
@@ -35,10 +62,11 @@ pub fn update_debug_window(canvas: &mut RaylibDrawHandle, interconnect: &Interco
         y_draw += 8 * SCALE;
         x_draw = 0;
     }
+    canvas.present();
 }
 
-pub fn display_tile(
-    canvas: &mut RaylibDrawHandle,
+fn display_tile(
+    canvas: &mut WindowCanvas,
     interconnect: &Interconnect,
     start_loc: u16,
     tile_num: u16,
@@ -48,26 +76,64 @@ pub fn display_tile(
     for tile_y in (0..16).step_by(2) {
         let addr: u16 = start_loc + (tile_num * 16) + tile_y;
 
-        // Get first byte
-        let b1: u8 = interconnect.read_mem(addr);
+        /*let b1: u8 = interconnect.read_mem(addr);
+            let b2: u8 = interconnect.read_mem(addr + 1);
 
-        // Get second byte
-        let b2: u8 = interconnect.read_mem(addr + 1);
+            for bit in (0..7).rev() {
+                let mut hi: u8 = b1 & (1 << bit) << 1;
+                let mut lo: u8 = b2 & (1 << bit);
 
-        for bit in (0..7).rev() {
-            let hi: u8 = !!(b1 & (1 << bit)) << 1;
-            let low: u8 = !!(b2 & (1 << bit));
+                hi = (hi == 0) as u8;
+                hi = (hi == 0) as u8;
+                hi = hi << 1;
 
-            let color: u8 = hi | low;
+                lo = (lo == 0) as u8;
+                lo = (lo == 0) as u8;
+
+                let color: u8 = hi | lo;
+
+                let new_x = (x as i32) + ((7 - bit) * SCALE);
+                let new_y = (y as i32) + ((tile_y as i32) / 2 * SCALE);
+
+                let w = SCALE as i32;
+                let h = SCALE as i32;
+                //println!("ADDR_VALUE: {:#X} ADDR1_VALUE {:#X} ADDR: {:#X} ADDR+1: {:#X} HI: {:#X} LO: {:#X} COLOR: {}",b1, b2, addr, addr +1, hi, lo, color);
+            canvas.draw_rectangle(new_x, new_y, w, h, TILE_COLORS[color as usize]);
+            p
+        }*/
+        // Get First BYTE
+        let second_byte: u8 = interconnect.read_mem(addr);
+
+        // Get Second BYTE
+        let first_byte: u8 = interconnect.read_mem(addr + 1);
+
+        // Index for tile color
+        let mut color: u8 = 0;
+
+        // Iterate over bits of first and second byte
+        for bit in (0..8).rev() {
+            let first_bit = (first_byte >> bit) & 1;
+            let second_bit = (second_byte >> bit) & 1;
+            if first_bit == 0 && second_bit == 0 {
+                color = 0
+            } else if first_bit == 0 && second_bit == 1 {
+                color = 1;
+            } else if first_bit == 1 && second_bit == 0 {
+                color = 2
+            } else {
+                color = 3;
+            }
 
             let new_x = (x as i32) + ((7 - bit) * SCALE);
             let new_y = (y as i32) + ((tile_y as i32) / 2 * SCALE);
 
-            let w = SCALE as i32;
-            let h = SCALE as i32;
+            let w = SCALE as u32;
+            let h = SCALE as u32;
 
-            canvas.draw_rectangle(new_x, new_y, w, h, tile_colors[color as usize]);
+            
+            canvas.set_draw_color(TILE_COLORS[color as usize]);
+            canvas.fill_rect(sdl2::rect::Rect::new(new_x, new_y, w, h));
+            
         }
     }
 }
-
