6b3ad0d2a5aeff4f1407230acee6e1056b2baec2
WuGambinos <lajuan.station@gmail.com>
2022-11-18 22:41:34 -0500

    Adjusted size of debug window

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,6 @@
-pub const SCREEN_WIDTH: u32 = 800;
-pub const SCREEN_HEIGHT: u32 = 600;
 pub const SCALE: i32 = 4;
+pub const SCREEN_WIDTH: u32 = 18 * 8 * (SCALE as u32);
+pub const SCREEN_HEIGHT: u32 = 28 * 8 * (SCALE as u32);
 
 pub const CLOCK_SPEED: usize = 4194304;
 pub const MAX_CYCLES_PER_FRAME: usize = (CLOCK_SPEED as f32 / 59.7275) as usize;
