f217279c184e2d19c093bdaaa965d29d849d4e3d
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2023-10-23 04:53:26 -0400

    Now reading keyboard input

--- rustboy-gui/src/main.rs
+++ rustboy-gui/src/main.rs
@@ -9,6 +9,7 @@ use clap::*;
 use env_logger::*;
 use imgui::{Condition, DrawListMut, ImColor32, Ui};
 use rustboy::constants::TILE_COLORS;
+use rustboy::interconnect::joypad::Key;
 use rustboy::interconnect::Interconnect;
 use rustboy::{
     constants::{X_RESOLUTION, Y_RESOLUTION},
@@ -30,6 +31,31 @@ struct Args {
     skip_boot: bool,
 }
 
+fn imgui_key_to_gb_key(keycode: imgui::Key) -> Option<Key> {
+    match keycode {
+        imgui::Key::RightArrow | imgui::Key::D => Some(Key::Right),
+        imgui::Key::LeftArrow | imgui::Key::A => Some(Key::Left),
+        imgui::Key::UpArrow | imgui::Key::W => Some(Key::Up),
+        imgui::Key::DownArrow | imgui::Key::S => Some(Key::Down),
+        imgui::Key::Z => Some(Key::A),
+        imgui::Key::X => Some(Key::B),
+        imgui::Key::Space => Some(Key::Select),
+        imgui::Key::Enter => Some(Key::Start),
+        _ => None,
+    }
+}
+
+const keys: [imgui::Key; 8] = [
+    imgui::Key::RightArrow,
+    imgui::Key::LeftArrow,
+    imgui::Key::UpArrow,
+    imgui::Key::DownArrow,
+    imgui::Key::Z,
+    imgui::Key::X,
+    imgui::Key::Space,
+    imgui::Key::Enter,
+];
+
 fn main() -> Result<(), anyhow::Error> {
     // Command Line Arguments
     let args = Args::parse();
@@ -46,8 +72,19 @@ fn main() -> Result<(), anyhow::Error> {
 
     let system = support::init(file!());
     system.main_loop(move |_, ui| {
-        gameboy.cpu.run(&mut gameboy.interconnect);
+        for key in keys {
+            if ui.is_key_down(key) {
+                if let Some(gb_key) = imgui_key_to_gb_key(key) {
+                    gameboy.interconnect.key_down(gb_key);
+                }
+            } else if ui.is_key_released(key) {
+                if let Some(gb_key) = imgui_key_to_gb_key(key) {
+                    gameboy.interconnect.key_up(gb_key);
+                }
+            }
+        }
 
+        gameboy.cpu.run(&mut gameboy.interconnect);
         //gui::memory_viewer(ui, &gameboy);
         gui::display_info(ui, &gameboy);
         gui::draw_tiles(ui, &gameboy.interconnect);
